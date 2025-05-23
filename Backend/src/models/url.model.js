6e1997f29603cf02659f021c7f5b8300e840cf2b
Vijay Patel <vijaypatel3207@gmail.com>
2025-05-23 22:54:16 +0530

    Url model is done

---
+++ Backend/src/models/url.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+  full_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
