508a6828136d7f5cab438ee1c138bcf08c288532
Vijay Patel <vijaypatel3207@gmail.com>
2025-07-04 23:56:33 +0530

    logout is done

---
+++ Frontend/src/api/LogoutApi.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from './AuthContext';
+
+const LOGOUT_URI = `${import.meta.env.VITE_API_BASE_URL}/auth/logout`;
+
+
+export const useLogout = () => {
+    const { setUser } = useContext(AuthContext);
+
+    const logoutUser = async() => {
+        try {
+            const response = await axios.post(LOGOUT_URI, 
+            { withCredentials: true});
+
+            setUser(response.data.user);
+            return response.data;
+        } catch (error) {
+            if (error.response && error.response.data) {
+            throw new Error(error.response.data.message || "Logout failed");
+            }
+            throw new Error("Logout failed");
+        }
+    }
+
+    return { logoutUser };
+}
