14d958944705ac4ada8ae388853490d59ec69fb1
Vijay Patel <vijaypatel3207@gmail.com>
2025-08-02 01:03:13 +0530

    new component for url header

---
+++ Frontend/src/components/Url/UrlsHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UrlsHeader = () => {
+  return (
+    <div className="mb-8 mt-12">
+      <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent mb-4">
+        My URLs
+      </h1>
+      <p className="text-gray-600 text-lg">
+        Manage and track your shortened URLs
+      </p>
+    </div>
+  );
+};
+
+export default UrlsHeader;
